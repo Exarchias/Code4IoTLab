43047a40dec9ca13559c9e77c1fcbf03d175328d
Robert Kristian Alm <robert.alm0018@stud.hkr.se>
2022-01-29 07:13:44 +0100

    ETHTask1to5wi

---
+++ For Windows/ETHTask1to5wi/client/app.js
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import LifeCycle from "./components/LifeCycle";
+import Measurements from "./components/Measurements";
+
+ReactDOM.render(
+    <div>
+    <Measurements/>
+    </div>,
+    document.getElementById("root")
+);
